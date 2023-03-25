8c74f60e61a6d7d7785a35d403eea5be2a17a58c
Frank Chang <frank.chang1000@gmail.com>
2023-03-25 16:13:54 -0400

    share

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import "rsuite/dist/rsuite.min.css";
+import "./Share.css";
+
 export default function Share() {
-	return <div>
-		<h1>Share</h1>
-	</div>
+  const [story, setStory] = useState("");
+
+  const handleStoryChange = (event) => {
+    setStory(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // PASS STORY TO BACKEND
+	
+  };
+
+  return (
+    <div className="share-container">
+      <h1>Share Your Story</h1>
+      <form className="share-form" onSubmit={handleSubmit}>
+        <label htmlFor="story">What do you want to talk about?</label>
+        <textarea
+          id="story"
+          name="story"
+          rows="5"
+          value={story}
+          onChange={handleStoryChange}
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
