dc07deaeabe830d323b6b0600b4eea193778efeb
Michael Fatemi <myfatemi04@gmail.com>
2023-03-25 16:03:44 -0400

    home page done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
-import { Button, ButtonGroup } from "rsuite";
-import "rsuite/dist/rsuite.min.css";
+import { useEffect } from "react";
 import "./Home.css";
+
 export default function Home() {
+	useEffect(() => {
+		requestAnimationFrame(() => {
+
+		});
+	}, []);
 	return (
-		<div className='button-container'>
-			<ButtonGroup vertical>
-				<Button appearance="primary">I want to talk.</Button>
-				<Button appearance="subtle">I want to share.</Button>
-			</ButtonGroup>
+		<div style={{ display: "flex", flexDirection: "column", margin: "4rem", alignItems: "center" }}>
+			<h1>Everyone's life flows a different way.</h1>
+			<div>
+				<span>What do you want to talk about?</span>
+				<input type="text" className="text-input" />
+				<a href="/share">Share a story</a>
+			</div>
 		</div>
 	);
 }
