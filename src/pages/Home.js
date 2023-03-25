021c9b93df582d1090323b3cad22b37a258e94b6
Frank Chang <frank.chang1000@gmail.com>
2023-03-25 19:34:45 -0400

    transparent background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,16 @@ export default function Home() {
 		});
 	}, []);
 	return (
-		<div style={{ display: "flex", flexDirection: "column", margin: "4rem", alignItems: "center" }}>
-			<h1>Everyone's life flows a different way.</h1>
-			<div>
-				<span>What do you want to talk about?</span>
-			</div>
-			<div className="buttons">
-				<a href="/talk" className="button"> Talk </a>
-				<a href="/share" className="button" style={{ marginLeft: "0.5rem" }}> Share a story </a>
+		<div className="main-container">
+			<div style={{ display: "flex", flexDirection: "column", margin: "4rem", alignItems: "center" }}>
+				<h1>Everyone's life flows a different way.</h1>
+				<div>
+					<span>What do you want to talk about?</span>
+				</div>
+				<div className="buttons">
+					<a href="/talk" className="button"> Talk </a>
+					<a href="/share" className="button" style={{ marginLeft: "0.5rem" }}> Share a story </a>
+				</div>
 			</div>
 		</div>
 	);
