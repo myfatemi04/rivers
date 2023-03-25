eebca7efa0a5bd3624d403dc6d1e329a929ced6b
Frank Chang <frank.chang1000@gmail.com>
2023-03-25 14:28:14 -0400

    button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
+import { ButtonGroup } from "rsuite";
+import { Button, ButtonToolbar } from 'rsuite';
+
 export default function Home() {
 	<link rel="stylesheet" href="Home.css"></link>
 	return (
 	  <div>
-		<h1>Home</h1>
-		<div class="button-container">
-		  <button class="big-button">I want to talk.</button>
-		  <button class="big-button">I want to view other stories.</button>
-		</div>
+		<button-container>
+			<ButtonToolbar>
+				<Button appearance="default" block>
+				I want to talk.
+				</Button>
+				<Button appearance="default" block>
+				I want to view other stories.
+				</Button>
+			</ButtonToolbar>
+		</button-container>
 	  </div>
 	);
   }
