14976de872c7d16e377e67119cf002831237f41c
Frank Chang <frank.chang1000@gmail.com>
2023-03-25 20:33:36 -0400

    update message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
 			<div style={{ display: "flex", flexDirection: "column", margin: "4rem", alignItems: "center" }}>
 				<h1>Everyone's life flows a different way.</h1>
 				<div>
-					<span>What do you want to talk about?</span>
+					<span>What brings you here today?</span>
 				</div>
 				<div className="buttons">
 					<a href="/talk" className="button"> Talk </a>
