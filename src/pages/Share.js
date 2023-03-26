db887e479355771de21dfe2735dd94dcf84edf2f
Tao Groves <>
2023-03-26 05:00:27 -0400

    commented out splitype

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 import { addStory } from "../api";
-import SplitType from "split-type";
+//import SplitType from "split-type";
 import "./Share.css";
 
 export default function Share() {
