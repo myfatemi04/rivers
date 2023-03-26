c238350cf5d92277cde1d407909a8f407350e8e8
Frank Chang <frank.chang1000@gmail.com>
2023-03-25 21:02:50 -0400

    one page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Share from "./pages/Share";
 import Talk from "./pages/Talk";
+import { useState } from "react";
+
 
 export default function App() {
   /*
   Linear gradient from #590898 to #FF560E at 45 degree angle
-  */
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
@@ -14,4 +15,28 @@ export default function App() {
       <Route path="/talk" element={<Talk />} />
     </Routes>
   </BrowserRouter>
-}
+  */
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "Home":
+        return <Home />;
+      case "Talk":
+        return <Talk />;
+      case "Share":
+        return <Share />;
+      default:
+        return <Home />;
+    }
+  };
+
+    return (
+      <div className="container">
+        <div className="animation"></div>
+        <div className="content">{renderPage()}</div>
+      </div>
+    );
+  }
+
+
