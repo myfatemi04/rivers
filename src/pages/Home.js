4c5615c43e32cb927fd001228aaaf51ea3230347
Tao Groves <>
2023-03-26 04:58:10 -0400

    fixed some css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
 					{!showTalkPage && */}
 				<motion.div initial={{ opacity: 1 }} exit={{ opacity: 0, transition: { type: "tween", duration: 0.15 } }} style={{ padding: "4rem", display: "flex", flexDirection: "column", alignItems: "center" }}>
 					<h1 style={{ margin: 0 }}>Everyone's life flows a different way.</h1>
-					<p style={{ fontSize: "1.25rem" }}>Rivers collects the world's stories so anyone experiencing challenging times can know they aren't alone. <b>100% free, no registration required.</b></p>
+					<p style={{ fontSize: "1.25rem" }}>Rivers collects the world's stories so anyone experiencing challenging times can know they aren't alone. <b>100% free and anonymous.</b></p>
 
 					<p style={{ fontSize: "1.5rem", marginBottom: "0.5rem" }}>
 						I want to...
