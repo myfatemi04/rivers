06a8b873605b559780c09af6e457907b2fc805c5
Tao Groves <>
2023-03-26 04:28:24 -0400

    Added fade-in to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,19 +34,24 @@ export default function Home() {
 
 	return (
 		<>
-			<div className="main-container" animate={{ transition: { ease: "easeIn", duration: 0.5 }, height: "auto" }}>
+			<AnimatePresence>
+			<motion.div className="main-container"
+						initial={{ opacity: 0}}
+						viewport={{ once: true }}
+						animate={{opacity: 1, transition: { delay: 0.7, duration: 1 }, height: "auto" }}>
 				{/* <AnimatePresence mode="wait">
 					{!showTalkPage && */}
 				<motion.div initial={{ opacity: 1 }} exit={{ opacity: 0, transition: { type: "tween", duration: 0.15 } }} style={{ padding: "4rem", display: "flex", flexDirection: "column", alignItems: "center" }}>
 					<h1 style={{ margin: 0 }}>Everyone's life flows a different way.</h1>
-					<p>Rivers collects the world's stories so anyone experiencing challenging times can know they aren't alone.<br />100% free, no registration required.</p>
+					<p>Rivers collects the world's stories so anyone experiencing challenging times can know they aren't alone.</p>
 					<span>What brings you here today?</span>
 					<div className="buttons">
 						<Link to="/talk" className="button"> Talk </Link>
 						<Link to="/share" className="button" style={{ marginLeft: "0.5rem" }}> Share a story </Link>
 					</div>
 				</motion.div>
-			</div>
+			</motion.div>
+			</AnimatePresence>
 		</>
 	);
 }
